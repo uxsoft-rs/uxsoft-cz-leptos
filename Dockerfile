e5412805e0d3b26d7cd4814d67a8d7151b387665
Jan Dryk <me@uxsoft.cz>
2024-02-04 20:19:15 +0100

    add github actions and docker build

--- Dockerfile
+++ Dockerfile
@@ -1,40 +1,30 @@
-FROM rust:alpine AS builder
-WORKDIR /build
+FROM rustlang/rust:nightly-alpine as builder
 
 RUN apk update && \
-	apk upgrade --no-cache && \
-	apk add pkgconfig libressl-dev musl-dev npm
+    apk add --no-cache bash curl npm libc-dev binaryen
+    # protoc openssl-dev protobuf-dev gcc git g++ libc-dev make binaryen
 
-RUN rustup default nightly 
-RUN rustup target add wasm32-unknown-unknown
+RUN npm install -g sass
 
 RUN cargo install --locked cargo-leptos
-RUN npm install tailwindcss -g
 
+# Add the WASM target
+RUN rustup target add wasm32-unknown-unknown
+
+WORKDIR /work
 COPY . .
 
-RUN npx tailwindcss -i style/tailwind.css -o style/generated.css --minify
-RUN cargo leptos build --release
+RUN cargo leptos build --release -vv
 
+FROM rustlang/rust:nightly-alpine as runner
 
-FROM alpine:3.18 AS runner
-WORKDIR /var/www/app
+WORKDIR /app
 
-RUN addgroup -S server && \
-	adduser -S www-data -G server && \
-	chown -R www-data:server /var/www/app
+COPY --from=builder /work/target/release/uxsoft-cz-leptos /app/
+COPY --from=builder /work/target/site /app/site
+COPY --from=builder /work/Cargo.toml /app/
 
-COPY --chown=www-data:server --from=builder /build/target/server/release/uxsoft-cz-leptos ./server/uxsoft-cz-leptos
-COPY --chown=www-data:server --from=builder /build/target/front/wasm32-unknown-unknown/release/uxsoft-cz-leptos.wasm ./front/uxsoft-cz-leptos.wasm
-COPY --chown=www-data:server --from=builder /build/target/site ./site
+EXPOSE $PORT
+ENV LEPTOS_SITE_ROOT=./site
 
-USER www-data
-
-ENV LEPTOS_OUTPUT_NAME "uxsoft-cz-leptos"
-ENV LEPTOS_SITE_ROOT "/var/www/app/site"
-ENV LEPTOS_ENV "PROD"
-ENV LEPTOS_SITE_ADDR "0.0.0.0:3000"
-
-EXPOSE 3000
-
-CMD ["./server/uxsoft-cz-leptos"]
+CMD ["/app/uxsoft-cz-leptos"]
--- src/app/routes/utilities/mod.rs
+++ src/app/routes/utilities/mod.rs
@@ -1,7 +1,41 @@
+use std::collections::HashMap;
+
+use gray_matter::{engine::YAML, Matter};
 use leptos::*;
 use leptos_router::*;
+use rust_embed::{EmbeddedFile, RustEmbed};
+use serde::Deserialize;
 
 use crate::app::components::markdown::Markdown;
+
+#[derive(Deserialize, Debug)]
+struct FrontMatter {
+    title: String,
+}
+
+#[derive(RustEmbed)]
+#[folder = "src/app/routes/utilities/"]
+#[include = "*.md"]
+#[include = "*.mdx"]
+#[exclude = "*.rs"]
+struct Asset;
+
+impl Asset {
+    fn files() -> HashMap<String, (FrontMatter, String)> {
+        Asset::iter()
+            .map(|f| {
+                let ef = Asset::get(f.as_ref()).unwrap();
+                let contents = std::str::from_utf8(ef.data.as_ref()).unwrap();
+
+                let matter = Matter::<YAML>::new();
+                let result = matter.parse(contents);
+                let front_matter: FrontMatter = result.data.unwrap().deserialize().unwrap();
+
+                (f.to_string(), (front_matter, contents.to_string()))
+            })
+            .collect()
+    }
+}
 
 pub struct Util<'a> {
     pub id: &'a str,
@@ -19,7 +53,15 @@ pub const UTILITIES: &'static [Util] = &[Util {
 pub fn UtilitiesPage() -> impl IntoView {
     view! {
         <div>
-            "Utilities" <ul>{UTILITIES.iter().map(|u| view! { <li>{u.name}</li> }).collect_view()}
+            "Utilities" // <ul>
+            // {Asset::iter().map(|f| view! { <li>{f.to_string()}</li> }).collect_view()}
+            // </ul>
+            <ul>
+                {Asset::files()
+                    .iter()
+                    .map(|(n, (f, c))| view! { <li>{&f.title} <br/> {c}</li> })
+                    .collect_view()
+                }
             </ul>
         </div>
     }
@@ -34,22 +76,8 @@ pub fn UtilityPage() -> impl IntoView {
         .unwrap_or_default();
 
     if let Some(util) = UTILITIES.iter().find(|i| i.id == id) {
-        view! { 
-            <div>
-                "Utility" 
-                {id}
-                {util.name}
-
-                <Markdown markdown={util.markdown}/>
-            </div> 
-        }
-    }
-    else
-    {
-        view! {
-            <div>
-                "Page not found"
-            </div>
-        }
+        view! { <div>"Utility" {id} {util.name} <Markdown markdown=util.markdown/></div> }
+    } else {
+        view! { <div>"Page not found"</div> }
     }
 }
